40d8bf9ece4f7d172900125f67d5a475e9db7d2a
hexinnov <htlp001@gmail.com>
2017-05-24 00:00:41 +0530

    Getting Ready

--- src/apiAIGoogleAssistant.js
+++ src/apiAIGoogleAssistant.js
@@ -27,10 +27,18 @@ console.log(req.body.result.action);
 //ActionName
     if(req.body.result.action==="DiagnosisTriggerIntent.GenderInput"){
       //Business Logic
-        DiagnosisTrigger(req,res)
+        DiagnosisTrigger(req,res);
+      }//fire Gender and Age Intent
+      else if(req.body.result.action==="DiagnosisTriggerIntent.GenderInput.FirstSymptom"){
+        SaySymptomTrigger(req,res);
+      }//fire Symptom Process Chain Intent
+      else if(req.body.result.action==="UniversalYesIntent"){
+        getYesResponse(req,res);
+      }//fire Symptom Process Chain Intent
 
-
-    }//fire Gender and Age Intent
+      else if(req.body.result.action==="UniversalNoIntent"){
+        getNoResponse(req,res);
+      }//fire Symptom Process Chain Intent
 });
 //---------------------------------------------------------------------------------------------
 //=================================PORT LISTENER===============================================
@@ -79,16 +87,414 @@ function DiagnosisTrigger(req,res){
 
         console.log("Exiting Diagnosis Trigger Google");
 }
+
+function SaySymptomTrigger(req,res){
+  console.log("Entering saySymptom Trigger Google");
+  var symptomString=req.body.result.parameters.inputSymptom;
+  global.symptomString=symptomString;
+  console.log("Input Symptom : "+symptomString);
+
+
+  SendToParse(function(parsingFirstBody){
+
+  console.log("Sending : "+global.parsedBody.mentions);
+  if(global.parsedBody.mentions.length==0)
+   {
+
+     console.log("NLP DID NOT RETURN SYMPTOM ID");
+     console.log(global.parsedBody);
+     var ResponseToSendBackInResponse="I didn\'t get that. Try rephrasing that symptom. Give me one symptom at a time.";
+     const assistant = new ApiAiApp({request: req, response: res});
+     SimpleResponseFallbackInBadSymptom(assistant,ResponseToSendBackInResponse);
+
+
+   }
+   else
+  {
+    console.log("NLP RETURNED SYMPTOM ID");
+    console.log(global.parsedBody);
+    var diagnosisSymptomName=global.parsedBody.mentions[0].name;
+    global.diagnosisSymptomName=diagnosisSymptomName;
+    console.log("NLP Diagnosed Symptom Name : "+diagnosisSymptomName);
+    var diagnosisSymptomId=global.parsedBody.mentions[0].id;
+    global.diagnosisSymptomId=diagnosisSymptomId;
+    console.log("NLP Diagnosed Symptom ID : "+diagnosisSymptomId);
+    var diagnosisSymptomStatus=global.parsedBody.mentions[0].choice_id;
+    global.diagnosisSymptomStatus=diagnosisSymptomStatus;
+    console.log("NLP Diagnosed Symptom Status : "+diagnosisSymptomStatus);
+    var diagnosisSymptomType=global.parsedBody.mentions[0].orth;
+    console.log("NLP Diagnosed Symptom Type : "+diagnosisSymptomType);
+
+    processSymptom(req, res);
+  }
+
+//  context.succeed(facebookResponse);
+  console.log("Ended Test");
+});
+
+}
+
+function getYesResponse(req,res){
+console.log("FEEDBACK TRUE FOR YES INPUT INTENT TRIGGERED");
+  if(global.useCaseFlag==1)
+  {//Diagnosis Module Yes
+    if(global.followUpCounter==1)
+      {
+        global.yesFlag=1;
+        processSymptom(req,res);
+
+        //buildSpeechletResponse sends to uppermost block for ssml response processing
+        //callback sends it back
+      }
+    }
+  }
+
+function getNoResponse(req,res){
+  console.log("FEEDBACK TRUE FOR YES INPUT INTENT TRIGGERED");
+
+    if(global.useCaseFlag==1)
+    {//Diagnosis Module No
+      if(global.followUpCounter==1)
+        {
+          global.yesFlag=2;
+          processSymptom(req,res);
+
+          //buildSpeechletResponse sends to uppermost block for ssml response processing
+          //callback sends it back
+        }
+      }
+    }//END NO DIAGNOSIS
+//===================================BUSINESS LOGIC API CALLS AND CALLBACKS========================================================
+function SendToParse(callback){
+
+  var options = { method: 'POST',
+    url: 'https://api.infermedica.com/v2/parse',
+    headers:
+    { 'postman-token': '785d8d42-c9fd-b137-d1da-e44cfa4d64f1',
+     'cache-control': 'no-cache',
+     'app-id': '03d4fd34',
+     'app-key': '97fdf41e07745fe24dc8a7f8dfdad177',
+     'content-type': 'application/json' },
+     body: { text: global.symptomString, include_tokens: false },
+     json: true };
+
+     request(options, function (error, response, body) {
+       if (error) throw new Error(error);
+
+       //console.log(body);
+       console.log(body);
+       global.parsedBody=body;
+       callback(global.parsedBody);
+      });
+}//GET DISEASE ID
+
+function SendToDiagnose(callback){
+  console.log("Going to Fire SendToDiagnose POST : "+JSON.stringify(global.buildBody));
+  var options = { method: 'POST',
+  url: 'https://api.infermedica.com/v2/diagnosis',
+  headers:
+  { 'postman-token': 'c85c7ea4-fc05-cd5c-2936-d20592218957',
+    'cache-control': 'no-cache',
+    'app-id': '03d4fd34',
+    'app-key': '97fdf41e07745fe24dc8a7f8dfdad177',
+    'content-type': 'application/json' },
+    body: global.buildBody,
+    json: true };
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      console.log("POST request");
+      console.log("Recieved Body : "+JSON.stringify(body));
+
+      global.diagnosisBody=body;
+      console.log("Number of Probable Conditions : "+global.diagnosisBody.conditions.length);
+      var bodyLength=global.diagnosisBody.conditions.length;
+      var i=0;
+      var index;
+      global.flag=0;
+      for(i=0;i<bodyLength;i++)
+      {
+        if(global.diagnosisBody.conditions[i].probability>=0.9000)
+        {
+          index=i;
+          global.index=index;
+          flag=1;
+          global.flag=flag;
+        }
+      }
+      //FOUND THE PROBABILTIES INDEX FOR HIGHER THAN 90 PERCENT
+      //-------------------------------------------------------
+      console.log("Probability Boolean : "+global.flag);
+
+
+       callback(diagnosisBody);
+      });
+}//PROCESS SYMPTOM
+
+function processSymptom(req,res){
+        console.log("Entering Process POST Google");
+
+        console.log("PROCESSING FOR FURTHER DIAGNOSIS INTENT TRIGGERED");
+         if(global.followUpCounter==1)
+         {
+             console.log("global.followUpCounter==1");
+             if(global.yesFlag==1)
+               {
+                 //YES WAS TRIGGERED, SO SYMPTOM IS PRESENT
+                 console.log("global.yesFlag==1");
+                 global.diagnosisSymptomStatus='present';
+
+                 global.result.push({id: global.followUpSymptomId, choice_id: global.diagnosisSymptomStatus});
+                 var buildBody= { sex: global.genderValue,
+                                  age: global.ageValueNumber,
+                                  evidence: global.result
+                                };
+                 global.buildBody=buildBody;
+                 console.log("Follow Up Counter 1 && global.yesFlag=1 (Present) : "+global.buildBody);
+
+               }
+
+             else if(global.yesFlag==2)
+               {
+                 //NO WAS TRIGGERED, SO SYMPTOM IS ABSENT
+                 console.log("global.yesFlag==2");
+                 global.diagnosisSymptomStatus='absent';
+
+                 global.result.push({id: global.followUpSymptomId, choice_id: global.diagnosisSymptomStatus});
+                 var buildBody= { sex: global.genderValue,
+                                  age: global.ageValueNumber,
+                                  evidence: global.result
+                                };
+                 global.buildBody=buildBody;
+                 console.log("Follow Up Counter 1 && global.yesFlag=2 (Absent) : "+global.buildBody);
+               }
+         }
+         else if(global.followUpCounter==0)
+         {
+             console.log("global.followUpCounter==0");
+             console.log("Body Prebuild Data on SexType : "+global.genderValue);
+
+             var result = [];
+             global.result=result;
+             global.result.push({id: global.diagnosisSymptomId, choice_id: global.diagnosisSymptomStatus});
+             var buildBody= { sex: global.genderValue,
+                              age: global.ageValueNumber,
+                              evidence: global.result
+                            };
+             console.log("Before Body Goes BuildBody Global : "+buildBody);
+             global.buildBody=buildBody;
+             console.log("Follow Up Counter 0 : "+JSON.stringify(global.buildBody));
+         }
+
+         console.log("Ready to send Request");
+
+         SendToDiagnose(function(buildBody){
+
+           if(global.diagnosisBody.question.type==='single')
+         //NIER POINT
+
+             {
+                 console.log("Single Type Question Returned");
+                 if(global.flag==1)
+                 {
+                     //console.log(body);
+
+                     console.log("HIGHEST FOUND");
+                     console.log("Name of disease : "+global.diagnosisBody.conditions[global.index].name);
+                     var finalDiseaseName=global.diagnosisBody.conditions[global.index].name;
+                     global.finalDiseaseName=finalDiseaseName;
+                     console.log("Id of disease : "+global.diagnosisBody.conditions[global.index].id);
+                     var finalDiseaseId=global.diagnosisBody.conditions[global.index].id;
+                     global.finalDiseaseId=finalDiseaseId;
+                     console.log("Probability of disease : "+global.diagnosisBody.conditions[global.index].probability);
+                     var finalProbability=global.diagnosisBody.conditions[global.index].probability;
+                     var probability=(finalProbability*100);
+                     global.probability=probability;
+                     console.log("Alexa will say : "+probability+" %");
+
+                     global.followUpCounter=1;
+                     global.yesFlag=0;
+                     sayFinalDiagnosis(req, res);//GOES TO SAY FINAL DIAGNOSIS
+                     //buildSpeechletResponse sends to uppermost block for ssml response processing
+                     //callback sends it back*/
+                 }
+                 else if(global.flag==0)
+                 {
+                     console.log("CONTINUE TO ASK QUESTIONS");
+                     console.log(global.diagnosisBody);
+                     var followUpQuestion=global.diagnosisBody.question.text;
+                     console.log("Follow Up Question Type : "+global.diagnosisBody.question.type);
+                     var followUpQuestionType=global.diagnosisBody.question.type;
+                     console.log("Follow Up Question : "+followUpQuestion);
+                     console.log("Follow Up Question ID : "+global.diagnosisBody.question.items[0].id);
+                     var followUpSymptomId=global.diagnosisBody.question.items[0].id;
+                     global.followUpSymptomId=followUpSymptomId;
+                     console.log("New Single Id to Push : "+global.followUpSymptomId);
+                     console.log("Follow Up Question Symptom Lookup Name : "+global.diagnosisBody.question.items[0].name);
+                     var ResponseToSendBackInResponse=followUpQuestion;
+                     const assistant = new ApiAiApp({request: req, response: res});
+                     basicCardFollowUpQuestion(assistant,ResponseToSendBackInResponse);
+                     //':ask' mode
+
+                     global.followUpCounter=1;
+                     global.yesFlag=0;
+
+                 }
+
+             }//FOR SINGLE TYPE QUESTIONS PROCESSING
+
+             else if(global.diagnosisBody.question.type==='group_single')
+             {
+               console.log("GROUP SINGLE TYPE HIT");
+               console.log(global.diagnosisBody);
+               //global.diagnosisBody=body;
+
+               processGroupDiagnosis(event,context);
+             }//FOR GROUP TYPE QUESTIONS PROCESSING
+
+             else if(global.diagnosisBody.question.type==='group_multiple')
+             {
+               console.log("GROUP MULTIPLE TYPE HIT");
+               console.log(global.diagnosisBody);
+               //global.diagnosisBody=body;
+
+               processGroupDiagnosis(event,context);
+             }//FOR GROUP TYPE QUESTIONS PROCESSING
+
+
+           });
+
+
+        console.log("Exiting POST Trigger Google");
+  }//END AGE AND GENDER INPUT function
+
+  function processGroupDiagnosis(req,res){
+    console.log("GROUP DIAGNOSIS FUNCTION TRIGGERED");
+
+    //console.log(body);
+    console.log(global.diagnosisBody.question.type);//group_multiple
+
+    console.log(global.diagnosisBody.question.text);//The followUp Question
+    var groupText=global.diagnosisBody.question.text;
+    console.log(global.diagnosisBody.question.items.length);
+    var groupTypeBodyLength=global.diagnosisBody.question.items.length;
+
+    global.groupIndex=0;
+    var i=0;
+    var speakCounter=0;
+    for(i=0;i<groupTypeBodyLength;i++)
+    {
+        speakCounter++;
+    }
+    console.log("Asking Question Number : "+speakCounter);
+    console.log(global.diagnosisBody.question.items[global.groupIndex].name);
+    var firstInitiateGroupFollowUpName=global.diagnosisBody.question.items[global.groupIndex].name;
+    console.log(global.diagnosisBody.question.items[global.groupIndex].id);
+    var firstInitiateGroupFollowUpId=global.diagnosisBody.question.items[global.groupIndex].id;
+    global.followUpSymptomId=firstInitiateGroupFollowUpId;
+    console.log("New Group Id to Push : "+global.followUpSymptomId);
+    global.groupIndex++;
+
+    var ResponseToSendBackInResponse=groupText+". Would you say "+firstInitiateGroupFollowUpName;
+    //Speak Out var groupText; That is the question : "How bad is the pain? Is it + "SEVERE"? "
+    const assistant = new ApiAiApp({request: req, response: res});
+    basicCardGroupTypeFollowUpQuestion(assistant,ResponseToSendBackInResponse);
+
+
+    global.followUpCounter=1;
+    global.yesFlag=0;
+
+    //speak out the First Group Follow Up
+  }//END AGE AND GENDER INPUT function
+
+  function sayFinalDiagnosis(req,res){
+    console.log("FINAL DIAGNOSED INTENT TRIGGERED");
+
+    var buildURL='https://api.infermedica.com/v2/conditions/'+  global.finalDiseaseId;
+    console.log("URL Built : "+buildURL);
+    var options = { method: 'GET',
+    url: buildURL,
+    headers:
+    { 'postman-token': '20cd3cf6-7a8e-9e62-3405-9603fd782d59',
+    'cache-control': 'no-cache',
+    'app-id': '03d4fd34',
+    'app-key': '97fdf41e07745fe24dc8a7f8dfdad177',
+    'content-type': 'application/json' } };
+
+    request(options,function (error, response, body) {
+      if (error) throw new Error(error);
+      console.log(body);
+      console.log(JSON.parse(body).name);
+      var conditionNameResult=JSON.parse(body).name;
+      console.log(JSON.parse(body).prevalence);
+      var conditionPrevalenceResult=JSON.parse(body).prevalence;
+      var conditionPrevalenceResult = conditionPrevalenceResult.charAt(0).toUpperCase() + conditionPrevalenceResult.slice(1);
+      var conditionPrevalenceResult=conditionPrevalenceResult.replace(/_/g,' ');
+      console.log(JSON.parse(body).acuteness);
+      var conditionAcutenessResult=JSON.parse(body).acuteness;
+      var conditionAcutenessResult = conditionAcutenessResult.charAt(0).toUpperCase() + conditionAcutenessResult.slice(1);
+      var conditionAcutenessResult=conditionAcutenessResult.replace(/_/g,' ');
+      console.log(JSON.parse(body).severity);
+      var conditionSeverityResult=JSON.parse(body).severity;
+      var conditionSeverityResult = conditionAcutenessResult.charAt(0).toUpperCase() + conditionAcutenessResult.slice(1);
+      var conditionSeverityResult=conditionAcutenessResult.replace(/_/g,' ');
+      console.log(JSON.parse(body).extras.hint);
+      var conditionAdviceResult=JSON.parse(body).extras.hint;
+
+      var ResponseToSendBackInResponse="Based on the symptoms you have given, there is a chance that you may have "+global.finalDiseaseName+". That\'s just what I can figure out. "+conditionAdviceResult;
+      //Speak Out var groupText; That is the question : "How bad is the pain? Is it + "SEVERE"? "
+      const assistant = new ApiAiApp({request: req, response: res});
+      basicCardFinalDiagnosis(assistant,ResponseToSendBackInResponse);
+      global.useCaseFlag=0;
+    });
+  }
+
+//---------------------------------------------------------------------------------------------------------------------------------
 //===================================RESPONSE FUNCTIONS BASED ON EACH INTENT FIRED=================================================
 function basicCardDiagnosisTrigger (app,ResponseToSendBackInResponse) {
   app.ask(app.buildRichResponse()
     .addSimpleResponse({speech: ResponseToSendBackInResponse, displayText: ''})
-    .addBasicCard(app.buildBasicCard(ResponseToSendBackInResponse) // Note the two spaces before '\n' required for a
-    .setSubtitle('This is a subtitle')
+    .addBasicCard(app.buildBasicCard(ResponseToSendBackInResponse)
+    .setSubtitle('Follow Up Question')
     .setTitle('SYMPTOM DIAGNOSIS: Say a symptom')
     .setImage(IMG_URL_AOG, 'Image alternate text'))
   );//BASIC CARD
 }
+
+function SimpleResponseFallbackInBadSymptom (app) {
+  app.ask(app.buildRichResponse()
+  .addSimpleResponse({speech: ResponseToSendBackInResponse, displayText: ''})
+  );//SIMPLE RESPONSE
+}
+function basicCardFollowUpQuestion (app,ResponseToSendBackInResponse) {
+  app.ask(app.buildRichResponse()
+    .addSimpleResponse({speech: ResponseToSendBackInResponse, displayText: ''})
+    .addBasicCard(app.buildBasicCard(ResponseToSendBackInResponse)
+    .setSubtitle('Diagnosis Question No : X')
+    .setTitle('SINGLE FOLLOW UP QUESTION:')
+    .setImage(IMG_URL_AOG, 'Image alternate text'))
+  );//BASIC CARD
+}
+function basicCardGroupTypeFollowUpQuestion (app,ResponseToSendBackInResponse) {
+  app.ask(app.buildRichResponse()
+    .addSimpleResponse({speech: ResponseToSendBackInResponse, displayText: ''})
+    .addBasicCard(app.buildBasicCard(ResponseToSendBackInResponse)
+    .setSubtitle('Diagnosis Group Question No : X')
+    .setTitle('GROUP FOLLOW UP QUESTION:')
+    .setImage(IMG_URL_AOG, 'Image alternate text'))
+  );//BASIC CARD
+}
+
+  function basicCardFinalDiagnosis (app,ResponseToSendBackInResponse) {
+    app.ask(app.buildRichResponse()
+      .addSimpleResponse({speech: ResponseToSendBackInResponse, displayText: ''})
+      .addBasicCard(app.buildBasicCard(ResponseToSendBackInResponse)
+      .setSubtitle('Final Answer : X')
+      .setTitle('FINAL DIAGNOSIS:')
+      .setImage(IMG_URL_AOG, 'Image alternate text'))
+    );//BASIC CARD
+}
+
 //-----------------------------------------------------------------------------------------------------------------------------
 //======================================ACTIONS ON GOOGLE RESPONSE TEMPLATES===================================================
 
